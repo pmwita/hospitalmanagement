3c4b8b046018e56f0f3875bd2da1e2b21fd7c1ba
pmwita <pmwita2017@gmail.com>
2024-04-06 23:15:18 +0300

    Add all my files

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['localhost','pmwita.pythonanywhere.com']
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
