da9c0be510359d8156a9e36ee75d47ca951d225c
pmwita <pmwita2017@gmail.com>
2024-04-08 20:43:12 +0300

    Add all my files

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -1,17 +1,22 @@
 import os
 import dj_database_url
 
+# Define the base directory of the project
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Define the directory where templates are stored
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
-# Remove the line STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
+# Set the secret key for the Django application
 SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 
+# Enable/disable debugging mode
 DEBUG = True
 
+# Set the allowed hosts for the application
 ALLOWED_HOSTS = ['*']
 
+# List of installed Django applications
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -23,6 +28,7 @@ INSTALLED_APPS = [
     'widget_tweaks',
 ]
 
+# Middleware classes used in the application
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -33,12 +39,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Root URL configuration
 ROOT_URLCONF = 'hospitalmanagement.urls'
 
+# Template configurations
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR, ],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -51,27 +59,15 @@ TEMPLATES = [
     },
 ]
 
+# WSGI application for deployment
 WSGI_APPLICATION = 'hospitalmanagement.wsgi.application'
 
-# # Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'hospitalmanagement',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
-
+# Database configuration (using environment variable DATABASE_URL)
 DATABASES = {
-    "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-
-# Password validation
+# Password validation settings
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -87,22 +83,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
+# Internationalization settings
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+# Static files settings
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Media settings
+# Media files settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Redirect URL after login
 LOGIN_REDIRECT_URL = '/afterlogin'
 
 # Email settings
